74f6b2b2a00aafdf65ef7497ed52f33023628163
jeremycardona <jeremy.cardona@upr.edu>
2016-02-22 01:30:55 -0400

    twitterdemo
    code path week 4

---
+++ TwitterDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TwitterDemo
+//
+//  Created by Gabriela A. Renta on 2/21/16.
+//  Copyright © 2016 Jeremy Cardona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func onLoginButton(sender: AnyObject) {
+    }
+
+
+}
+
